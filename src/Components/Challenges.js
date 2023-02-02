9e98a86e1b9a95c32e17a6d525fcf73296db5966
poojitha-angular <poojithashagnati@gmail.com>
2023-02-02 18:50:37 +0530

    Uploading Committed

---
+++ src/Components/Challenges.js
@@ -0,0 +1,27 @@
+import './css/challenges.css'
+function Challenges(){
+    return(
+        <div class="main-ctn">
+            <div className="title-ctn">
+                <h1 align="center">Challenges </h1>
+
+            </div>
+            <div className="pro-ctn">
+                <h2>Programming</h2>
+                <div className="pro-cards">
+                    <div className="column">
+                        <div class="card">
+                            <p>Hello world!</p>
+                            <p>15</p>
+
+                        </div>
+
+                    </div>
+                    </div>
+
+            </div>
+
+        </div>
+
+    )
+}export default Challenges;
