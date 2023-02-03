309e8efbcf6d38744cbeb074977e5b7aa05454f3
poojitha-angular <poojithashagnati@gmail.com>
2023-02-03 13:47:59 +0530

    challenges page css modified

--- src/Components/Challenges.js
+++ src/Components/Challenges.js
@@ -3,25 +3,78 @@ function Challenges(){
     return(
         <div class="main-ctn">
             <div className="title-ctn">
-                <h1 align="center">Challenges </h1>
+                <h1 align="center"> Welcome to the Challenges </h1>
 
             </div>
-            <div className="pro-ctn">
-                <h2>Programming</h2>
-                <div className="pro-cards">
+             
+                <div className="cards">
                     <div className="column">
                         <div class="card">
-                            <p>Hello world!</p>
-                            <p>15</p>
-
+                            <p class="title-tag">Certificate program in </p>
+                            <p>Product Managemnet</p>
                         </div>
-
                     </div>
+                    <div className="column">
+                        <div class="card">
+                            <p class="title-tag">Certificate program in </p>
+                            <p>Business Analytics</p>
+                        </div>
                     </div>
+                    <div className="column">
+                        <div class="card">
+                            <p class="title-tag">Accelarator program in  </p>
+                            <p>Data Analytics</p>
+                        </div>
+                    </div>
+                   
+                </div>   
+                <div className="cards">
+                    <div className="column">
+                        <div class="card">
+                            <p class="title-tag">Certificate program in </p>
+                            <p>Kubernetes</p>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div class="card">
+                            <p class="title-tag">Accelarator program in </p>
+                            <p>AWS Architecture</p>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div class="card">
+                            <p class="title-tag">Certificate program in </p>
+                            <p>Business Managemnet</p>
+                        </div>
+                    </div>
+                   
+                </div> 
+                <div className="cards">
+                    <div className="column">
+                        <div class="card">
+                            <p class="title-tag">Accelarator program in </p>
+                            <p> Finance</p>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div class="card">
+                            <p class="title-tag">Certificate program in </p>
+                            <p>Apache spark</p>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div class="card">
+                            <p class="title-tag">Certificate program in </p>
+                            <p>Apache Kafka fundamentals</p>
+                        </div>
+                    </div>
+                   
+                </div>                
+        </div>        
 
-            </div>
-
-        </div>
+         
+           
+        
 
     )
 }export default Challenges;
